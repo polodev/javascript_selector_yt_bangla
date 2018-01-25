594a1d36aacf6a959663d9afd35c204df55b95a0
polo dev <polodev10@gmail.com>
2018-01-25 09:30:41 +0600

    added js selector

---
+++ script.js
@@ -0,0 +1,13 @@
+var hello = document.getElementById('hello');
+var world = document.getElementsByClassName('world');
+var ul = document.getElementsByTagName('ul');
+var ul1 = ul[0];
+var world1 = world[0];
+
+document.querySelector('any css selector') //single element
+document.querySelectorAll('any css selector') //array of  element
+
+
+// 2 selector 
+// single element 
+// array of elements 
